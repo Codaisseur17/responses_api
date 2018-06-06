a84e003227fab9d7a4989f9bc455080264970953
b54690 <belafonte@hotmail.co.uk>
2018-06-06 23:21:24 +0200

    logic for get request by id included

--- src/resp/controller.ts
+++ src/resp/controller.ts
@@ -10,24 +10,67 @@ export default class ResponsesController {
   async newResponse(
       @Body() response: Responses
   ) {
+    const newReponse = await Responses.create(response).save()
     return response.save()
   }
 
-@Post('/questions')   
-@HttpCode(201)
-async newQuestion(
-    @Body() questions: Questions
-) {
-  return questions.save()
-}
-}
+  @Post('/questions')   
+  @HttpCode(201)
+  async newQuestion(
+      @Body() questions: Questions
+  ) {
+    return questions.save()
+  }
+  
+  @Get('/questions')
+  async getCorrectAnswers() {
+    const quiz = await Questions.find()
+    const meow = quiz.map(value => value.correctAnswer)
+    return {meow}
+    }
 
-// @Get('/questions/:id')
-// async getCorrectAnswers(
-// @Param('id') quizId: number
-// ) {
-//   const question = await Questions.findOne(quizId)
-//   }
+  @Get('/questions/:quizId')
+  async getQuestionByQuiz( 
+    @Param('quizId') quizId: number
+  ){
+    const quiz = await Questions.find({where: {quizId}})
+    const meow = quiz.map(value => value.correctAnswer)
+    console.log(meow)
+    return {meow}
+    }
+  }
+
+  // @Get('/responses/user/:user_id')
+  // getResponseUserId(
+  //     @Param('user_id') user_id:number
+  // ){
+  //     return Responses.findOne({where: {user_id}})
+  // }
+
+
+//   array1 = ["A" , "B", "C", "D", "A", "D", "E"]
+// array2 = ["A" , "C", "C", "C", "A", "X", "E"]
+
+
+// const findMatch = (array1, array2) => {
+//   var res = []
+//     for (var i = 0; i < array1.length; i++) {
+//       if (array1[i] === array2[i]) {
+//           res.push(i);
+//           //console.log(i) 
+//       }
+//     }
+//   var uniqueArray = res.map(function(index) {
+//   // console.log(index);
+//   return array1[index]
+//   })
+//   return uniqueArray
+// }
+
+// console.log(findMatch(array1,array2))
+
+
+
 
 
 // @Patch('/responses/:id')
