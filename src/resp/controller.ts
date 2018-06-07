de2f5b1125b2ca8b3d0701521e6c62ca0d87f8c7
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-06-07 09:34:27 +0200

    getting back user answers as an array

--- src/resp/controller.ts
+++ src/resp/controller.ts
@@ -22,56 +22,31 @@ async newQuestion(
 }
 
 @Get('/questions')
- async getCorrectAnswers(
- ) {
-    const quiz = await Questions.find()
-    // console.log(quiz, "hello")
-    const meow = quiz.map(value => value.correctAnswer)
-    console.log(meow, "bye")
-    return {quiz}
-  //  const question = await Questions.findOne(quizId)
+async getCorrectAnswers() {
+  const quiz = await Questions.find()
+  const meow = quiz.map(value => value.correctAnswer)
+  return {meow}
+  }
+
+@Get('/questions/:quizId')
+async getQuestionByQuiz( 
+  @Param('quizId') quizId: number
+){
+  const quiz = await Questions.find({where: {quizId}})
+  const meow = quiz.map(value => value.correctAnswer)
+  console.log(meow)
+  return {meow}
   }
 
 @Get('/responses')
  async getResponse(
  ) {
-    const whatthehell = await Responses.find()
-    console.log(whatthehell, "hello")
-    // const bark = await Responses.answers
-    // const bark = Object.entries(response)[0][1]
-    // const bark2 = Object.entries(bark)[3][1]
-    // const bark3 = Object.entries(bark2)[1]
-    // const bark = response.map(value => value.correctAnswer)
-    // console.log(bark, "bye")
-    return {whatthehell}
-  //  const question = await Questions.findOne(quizId)
+    const responses = await Responses.find()
+    const bark = responses[0]['input'].map(value => value.userAnswer)
+    console.log(bark, "bye")
+
+
+    return {responses}
+
   }
 }
-
-// (Object.entries(quiz)[0])
-
-
-
-// const answers=[{"1":"a"},{"2":"d"},{"3":"c"}]
-// const correctAnswer= 
-
-// @Get('/questions/:id')
-// async getCorrectAnswers(
-// @Param('id') quizId: number
-// ) {
-//   const question = await Questions.findOne(quizId)
-//   }
-
-
-// @Patch('/responses/:id')
-// giveScore(
-//   @Param('id') id: number,
-//   @Body() body: Partial<Response>
-// ) {
-//   const 
-// }
-// }
-
-
-
-// .map(value => value.correctAnswer)
