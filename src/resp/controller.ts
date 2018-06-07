c435f3539cb772db5efbe9f186ff3fd883655d07
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-06-07 11:21:29 +0200

    getting there with the score

--- src/resp/controller.ts
+++ src/resp/controller.ts
@@ -1,63 +1,94 @@
-import { JsonController, Post, HttpCode, Body, Param, Get } from 'routing-controllers'
+import { JsonController, Post, HttpCode, Body, Param, Get, Patch } from 'routing-controllers'
 import {Responses, Questions } from './entity'
-// import Questions from '../quizzes/src/controller'
 
 @JsonController()
 export default class ResponsesController {
 
 @Post('/responses')
 @HttpCode(201)
-async newResponse(
+  async newResponse(
 @Body() response: Responses
 ) {
-const newReponse = await Responses.create(response).save()
-return response.save()
+  const newReponse = await Responses.create(response).save()
+  return response.save()
 }
 
 @Post('/questions')
 @HttpCode(201)
-async newQuestion(
-@Body() questions: Questions
+  async newQuestion(
+  @Body() questions: Questions
 ) {
-return questions.save()
+  return questions.save()
 }
 @Get('/questions')
-async getCorrectAnswers() {
-const quiz = await Questions.find()
-const meow = quiz.map(value => value.correctAnswer)
-return {meow}
+  async getCorrectAnswers() {
+  const quizzes = await Questions.find()
+  // const meows = quizzes.map(value => value.correctAnswer)
+  return {quizzes}
 }
 
 @Get('/questions/:quizId')
-async getQuestionByQuiz(
-@Param('quizId') quizId: number
+  async getQuestionByQuiz(
+  @Param('quizId') quizId: number
 ){
-const quiz = await Questions.find({where: {quizId}})
-const meow = quiz.map(value => value.correctAnswer)
-console.log(meow)
-return {meow}
+  const quiz = await Questions.find({where: {quizId}})
+  // const meow = quiz.map(value => value.correctAnswer)
+  // console.log(meow)
+return {quiz}
 }
 
 @Get('/responses')
-async getResponse(
+  async getResponse(
 ) {
-const responses = await Responses.find()
-const bark = responses[0]['input'].map(value => value.userAnswer)
-console.log(bark, "bye")
-
-
+  const responses = await Responses.find()
+  // const barks = responses[0]['input'].map(value => value.userAnswer)
+  // console.log(barks, "bye")
 return {responses}
 }
 
 @Get('/responses/:quizId')
-async getResponseById(
-@Param('quizId') quizId: number
+  async getResponseById(
+  @Param('quizId') quizId: number
 ) {
-const response = await Responses.find({where: {quizId}})
-const bark = response[0]['input'].map(value => value.userAnswer)
-console.log(bark, "bye")
-
-
+  const response = await Responses.find({where: {quizId}})
+  // const bark = response[0]['input'].map(value => value.userAnswer)
+  // console.log(bark, "bye")
 return {response}
 }
+
+@Get('/responses/:quizId')
+  async giveScoreById(
+    @Param('quizId') quizId: number,
+    @Body() update: Partial<Responses>
+  ) {
+    const quiz = await Questions.find({where: {quizId}})
+    const meow = quiz.map(value => value.correctAnswer)
+    // console.log(meow, "hello")
+    
+    const response = await Responses.find({where: {quizId}})
+    const bark = response[0]['input'].map(value => value.userAnswer)
+    // console.log(bark, "bye")
+
+    const findMatch = (meow, bark) => {
+      let res = []
+        for (var i = 0; i < meow.length; i++) {
+          if (meow[i] === bark[i]) {
+              res.push(i);
+          }
+        }
+      var uniqueArray = res.map(function(index) {
+      return meow[index]
+      })
+      const score =  uniqueArray.length
+      console.log(score, "hello")
+    }
+
+    console.log(findMatch(meow,bark), "what")
+
+    
+    return {meow, bark}
+  }
+
 }
+
+//Response.merge(response, update).save()
