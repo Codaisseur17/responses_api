0d7287cf42bc00762a5d79541b2001ee0f533581
adambelton <thelastgreatcynic@gmail.com>
2018-06-08 09:17:03 +0200

    post responses returns response object with score

--- src/resp/controller.ts
+++ src/resp/controller.ts
@@ -1,127 +1,121 @@
-import { JsonController, Post, HttpCode, Body, Param, Get, Patch, NotFoundError } from 'routing-controllers'
-import {Responses, Questions } from './entity'
+import {
+  JsonController,
+  Post,
+  HttpCode,
+  Body,
+  Param,
+  Get
+  //   Patch
+  //   NotFoundError
+} from 'routing-controllers'
+import { Responses, Questions } from './entity'
 import * as request from 'superagent'
 
 const quizzesUrl = process.env.QUIZZES_URL || 'http://quizzes:4001'
 
 @JsonController()
 export default class ResponsesController {
-
-@Post('/responses')
-@HttpCode(201)
-async newResponse(
-@Body() response: Responses
-) {
-  const newReponse = await Responses.create(response).save()
-
-  const quiz = await request
+  @Post('/responses')
+  @HttpCode(201)
+  async newResponse(@Body() response: Responses) {
+    const questions = await request
       .get(`${quizzesUrl}/quizzes/${response.quizId}`)
-      .end()
+      .then(result => {
+        return result.body.questions
+      })
       .catch(error => console.log(error))
 
-      // console.log(request, "meow")
+    const responses = response.input
 
-      // const newReponse = await Responses.create(response).save()
-      const responses = response.input
-      // const questions = quiz.questions
-      console.log(response.request.input, "i did this!")
+    const score = responses
+      .map(res => {
+        return res.userAnswer ===
+          questions.find(element => element.id === res.id).correctAnswer
+          ? 1
+          : 0
+      })
+      .reduce((a, b) => {
+        return a + b
+      }, 0)
 
-// const score = responses.map(res => {
-//   res.userAnswer === questions.find(element => element.id === res.id).correctAnswer ? 1 : 0
-// }).reduce((a,b) => {a + b}, 0)
+    const newResponse = await Responses.create(response).save()
 
-// console.log(score)
+    newResponse.score = score
 
-    // console.log(`responses: ${JSON.stringify(responses)}`)
-    // console.log(`questions: ${JSON.stringify(questions)}`)
+    return newResponse.save()
+  }
 
-return { quiz }
-}
+  @Post('/questions')
+  @HttpCode(201)
+  async newQuestion(@Body() questions: Questions) {
+    return questions.save()
+  }
 
-@Post('/questions')
-@HttpCode(201)
-async newQuestion(
-@Body() questions: Questions
-) {
-return questions.save()
-}
+  @Get('/questions')
+  async getCorrectAnswers() {
+    const quizzes = await Questions.find()
+    return { quizzes }
+  }
 
+  @Get('/questions/:quizId')
+  async getQuestionByQuiz(@Param('quizId') quizId: number) {
+    const quiz = await Questions.find({ where: { quizId } })
+    return { quiz }
+  }
 
-@Get('/questions')
-async getCorrectAnswers() {
-const quizzes = await Questions.find()
-return {quizzes}
-}
+  @Get('/responses')
+  async getResponse() {
+    const responses = await Responses.find()
+    return { responses }
+  }
 
-@Get('/questions/:quizId')
-async getQuestionByQuiz(
-@Param('quizId') quizId: number
-){
-const quiz = await Questions.find({where: {quizId}})
-return {quiz}
-}
+  @Get('/responses/:quizId')
+  async getResponseById(@Param('quizId') quizId: number) {
+    const response = await Responses.find({ where: { quizId } })
+    return { response }
+  }
 
-@Get('/responses')
-async getResponse(
-) {
-const responses = await Responses.find()
-return {responses}
-}
+  //   @Patch('/responses/:quizId')
+  //   async giveScoreById(@Param('quizId') quizId: number, @Body() update: any) {
+  //     console.log(update, '<---update')
+  //     let score
+  //     const quiz = await Questions.find({ where: { quizId } })
+  //     const meow = quiz.map(value => value.correctAnswer)
 
-@Get('/responses/:quizId')
-async getResponseById(
-@Param('quizId') quizId: number
-) {
-const response = await Responses.find({where: {quizId}})
-return {response}
-}
+  //     const response = await Responses.find({ where: { quizId } })
+  //     var responseobj = response.reduce(function(acc, cur, i) {
+  //       acc[i] = cur
+  //       return acc
+  //     }, {})
+  //     console.log(responseobj[0].input[0].userAnswer, 'sup')
+  //     if(!response[0].input.userAnswer) throw new NotFoundError
+  //     const bark = response[0].input.map(value => value.userAnswer)
+  //     console.log(bark, "whatsupp")
+  //     const findMatch = (meow, bark) => {
+  //     let res:any = []
+  //     for (let i = 0; i < meow.length; i++) {
+  //     if (meow[i] === bark[i]) {
+  //     res.push(i);
+  //     console.log(res, "whaaat")
+  //     }
+  //   }
+  //   const uniqueArray = res.map(function(index) {
+  //   return meow[index]
+  //   })
+  //   score = uniqueArray.length
+  //   console.log(score, "hello")
+  //   return {score}
+  //   }
+  //   response[0].score = update.update.score
+  //   // console.log(response[0],"RESPONSE NEW")
 
-@Patch('/responses/:quizId')
-async giveScoreById(
-@Param('quizId') quizId: number,
-@Body() update: any
-) {
-console.log(update,"<---update")
-let score
-const quiz = await Questions.find({where: {quizId}})
-const meow = quiz.map(value => value.correctAnswer)
+  //   console.log(findMatch(meow,bark), "what")
 
-const response = await Responses.find({where: {quizId}})
-var responseobj = response.reduce(function(acc, cur, i) {
-acc[i] = cur;
-return acc;
-}, {});
-console.log(responseobj[0].input[0].userAnswer, "sup")
-// if(!response[0].input.userAnswer) throw new NotFoundError
-// const bark = response[0].input.map(value => value.userAnswer)
-// console.log(bark, "whatsupp")
-// const findMatch = (meow, bark) => {
-// let res:any = []
-// for (let i = 0; i < meow.length; i++) {
-// if (meow[i] === bark[i]) {
-// res.push(i);
-// console.log(res, "whaaat")
-// }
-}
-// const uniqueArray = res.map(function(index) {
-// return meow[index]
-// })
-// score = uniqueArray.length
-// console.log(score, "hello")
-// return {score}
-// }
-// response[0].score = update.update.score
-// // console.log(response[0],"RESPONSE NEW")
+  //   return await response[0].save()
+  //   // return Responses.merge(score, update)
 
-// console.log(findMatch(meow,bark), "what")
-
-// return await response[0].save()
-// // return Responses.merge(score, update)
-
-
-// return {score}
-// }
-
+  //   return {score}
+  //   }
 }
 
 // http patch :4002/responses/1 update:='{"id": 4,"input": [{"id": 1,"userAnswer": "C"},{"id": 2,"userAnswer": "C"},{"id": 3,"userAnswer": "D"}],"quizId": 1,"userId": 1, "score":2 }'
